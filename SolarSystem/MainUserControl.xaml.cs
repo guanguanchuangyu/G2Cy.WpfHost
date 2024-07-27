24de97b4e1dd819de9d62e53e4189405f7dc0069
ggcy <3246907890@qq.com>
2024-07-28 00:29:41 +0800

    [插件]-添加独立进程插件

---
+++ SolarSystem/MainUserControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SolarSystem
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainUserControl : UserControl
+    {
+        OrbitsCalculator _data = new OrbitsCalculator();
+        public MainUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
